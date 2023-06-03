d626e6d9467dde2129e640f857ad7109b8eba1fa
luisperezlemus <lperezle@asu.edu>
2023-06-02 22:26:02 -0700

    Songs fetching and list display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "ywzbhtklinfjxjqrcxbt.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
